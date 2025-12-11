1f9f789dc5ad1b907557173cd2bb3620c3cebd22
Jeremy-borderieux <jeremybor@orange.fr>
2025-12-10 17:00:42 -0800

    Jeremy annotated the scripts and pushed the latest model version of the models and figures. Waiting for funcgroup data

--- scripts/01_Diversity.R
+++ scripts/01_Diversity.R
@@ -1,10 +1,12 @@
-library(data.table)
-library(brms)
-library(ggplot2)
-library(stringr)
-library(vegan)
-## how does trampling affect species richness ?
+##### packages ####
+library(data.table) # faster data.frame
+library(brms) # bayesian model fitting using stan
+library(ggplot2) # plots
+library(stringr) # string manipulation
+library(vegan) # diversity indices computation
+#### how does trampling affect species richness ? ####
 
+## reading the csv, cleaning some values
 data_t <- fread(file.path("data","Trampling_Data_2024_Clean_nozeros.csv"))
 data_long <- melt(data_t,id.vars = colnames(data_t)[1:9]) 
 data_long[,value:=ifelse(value == "*","",value)]
@@ -13,73 +15,71 @@ data_long[,value:=as.numeric(value)]
 data_long[,value:=ifelse(is.na(value),0 ,value)]
 data_long[,.N,by = variable]
 
+## removing "species" not of interest
 data_long <- data_long[!variable%in% c("soil","litter","rock","crust","bryosp2","bryosp1"),]
 
-
-ggplot(data_long,aes(x = value,fill = treatment))+
-  geom_histogram(position = "dodge")+
-  facet_wrap(~ variable,scales = "free_y")+
-  theme_classic()
-
-
 table(data_long$value)
+table(data_long$variable)
 
 #### Richness ####
-data_richness <- data_long[,.(richness = sum(value!=0), shannon = diversity(value,"shannon")), by = eval(colnames(data_long)[1:9])]
-data_richness[,altitude_scaled := scale (altitude)]
-data_richness[,site := substr(transect,1,2)]
+## compute richness as species present in a subplot, and shannon diversity
+data_richness <- data_long[,.(richness = sum(value!=0), 
+                              shannon = diversity(value,"shannon")), by = eval(colnames(data_long)[1:9])]
+data_richness[,altitude_scaled := scale(altitude)] # center and scale the predictor incase we want to use it
+data_richness[,site := substr(transect,1,2)]## getting the site variable back with string manipulation
+data_richness[,transect_pair := paste0(site,"_",str_pad(trans.pair, width=2, side="left", pad="0"))]# naming the transect for plots
 
 table(data_richness$transect)
 table(data_richness$treatment)
 table(data_richness$commun)
 table(data_richness$trans.pair)
 
+## the distribution look like poisson or negbinomial
 ggplot(data_richness,aes(x = richness,fill = transect))+
   facet_wrap(~treatment)+
   geom_histogram(binwidth = 1)+
   theme_classic()
 
-library(brms)
+## Bayesian model 
 
-prior_rich <- set_prior("normal(2,0.25)",class = "Intercept")
-prior_rich <- c(prior_rich,set_prior("student_t(3, 0.5, 0.5)", class = "sd", group = "trans.pair"))
-prior_rich <- c(prior_rich,set_prior("student_t(3, 0.5, 0.5)", class = "sd", group = "site"))
-prior_rich <- c(prior_rich,set_prior("student_t(3, 0.5, 0.5)", class = "sd", group = "transect"))
-prior_rich <- c(prior_rich,set_prior("normal(500,300)", class = "shape" ))
+# setting priors, knowledge we know already to aid the fit
+prior_rich <- set_prior("normal(2,0.25)",class = "Intercept") #I already know the mean species richness 
+# in control plot is around exp(2)
+prior_rich <- c(prior_rich,set_prior("normal(500,20)", class = "shape" ))#I fix the shape paramter very hight to help the 
+# covnergence, as this parameter can go to infinity without any improvment of the fit
 
-
-data_richness[,.N, by = .(treatment,site,trans.pair,transect,altitude_scaled)]
-data_richness[,transect_pair := paste0(site,"_",str_pad(trans.pair, width=2, side="left", pad="0"))]
-
-default_prior(model_rich)
-
-model_rich <-  brm(richness ~ treatment  +(1+ |site) +  (1+ treatment|trans.pair) +(1|transect) ,
+# 1 means varying intercept(control richness) across site / pair / transect.
+# 1 + treatment means varying effect of treatment across pairs
+model_rich <-  brm(richness ~ treatment  +(1|site) +  (1 + treatment|trans.pair) +(1|transect) ,
                                   data = data_richness,
-                                  family = negbinomial(),
-                                  backend = "cmdstanr", 
-                                  iter = 2000,
-                                  prior = prior_rich,
-                                  warmup = 500,
-                                  chains = 3,
-                                  cores = 3,
-                                  control = list(adapt_delta = 0.98),
-                                  threads = threading(3),
-                                  init = 0,
-                                  file = file.path("model","richness_model")  )
+                                  family = negbinomial(), # distribution family
+                                  backend = "cmdstanr", # Need to be installed, use backend = "rstan" works aswell
+                                  iter = 2000, # number of iteration of the algo per chains (models)
+                                  prior = prior_rich, # the priors
+                                  warmup = 500, # number of iterations discarded 
+                                  chains = 3, # number of model to fit, they need to converge to a unique solution
+                                  cores = 3, # using a CPU core per model to fasten the computation
+                                  control = list(adapt_delta = 0.99), # increase the computation time to better estimate parameters, need if divergent transition
+                                  threads = threading(3), # this number can go up or down to fasten computation,
+                                  init = 0, # lower the risk of crash
+                                  file = file.path("model","richness_model_4")) # where the model is stored
+# will load the model instead of fitting one if the file already exists
 
-pp_check(model_rich)
-summary(model_rich)
-plot(model_rich)
+pp_check(model_rich) # posterio predictive check to make sur we fit the real distribution
+summary(model_rich) # summary of fixed and random effects
+plot(model_rich) # checking the convergence: fuzzy caterpillar, the chains have converged
 
 sjPlot::plot_model(model_rich,type = "pred",terms = "treatment")
 
+## getting the model prediction back for every transect
 preds_rich <- fitted(model_rich,
                      newdata = data_richness[,.N, by = .(treatment,site,trans.pair,transect,altitude_scaled)],
-                     re_formula = ~  (1 |site) + (1+ treatment|trans.pair)  +(1|transect))
+                     re_formula = ~  (1 |site) + (1+ treatment|trans.pair)  +(1|transect)) # I specifiy here the heirarchy I want in the preds
 
 
 preds_rich <- cbind(preds_rich,data_richness[,.N, by = .(treatment,site,trans.pair,transect_pair,transect,altitude_scaled)])
 
+# plot of pred vs real div
 (rich_pred_plot <- ggplot(data_richness,aes (x = transect_pair, y = richness , fill = treatment,group =treatment))+
   theme_classic()+
     theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1))+
@@ -92,15 +92,16 @@ preds_rich <- cbind(preds_rich,data_richness[,.N, by = .(treatment,site,trans.pa
   labs(x = "Transect pair", y = "Species richness", color = lab_trt <- "Position along\nthe trail",fill = lab_trt)
 )
 
+# export
 ggsave(file.path("figure","Fig_richness_transect.jpg"),rich_pred_plot,unit = "cm",width = 18,height = 11,dpi = 300)
+
 ggplot(data_richness,aes( x = treatment, y = richness,fill = site))+
   geom_violin()+
   theme_classic()
 
 
 
-#### Shanon div ####
-data_richness[,transect_pair := paste0(site,"_",str_pad(trans.pair, width=2, side="left", pad="0"))]
+#### how does trampling affect Shannon diversity? ####
 
 ggplot(data_richness,aes(x = shannon,fill = transect))+
   facet_wrap(~treatment)+
@@ -125,8 +126,8 @@ model_shannon <-  brm(shannon ~ treatment  +(1 |site) +  (1+ treatment|trans.pai
                    control = list(adapt_delta = 0.99),
                    threads = threading(3),
                    init = 0,
-                   file = file.path("model","shannon_model")  )
-data_richness
+                   file = file.path("model","shannon_model"))
+
 conditional_effects(model_shannon,"treatment")
 pp_check(model_shannon)+coord_cartesian(xlim = c(0,3))
 summary(model_shannon)
@@ -139,6 +140,7 @@ preds_shannon <- fitted(model_shannon,
 
 preds_shannon <- cbind(preds_shannon,data_richness[,.N, by = .(treatment,site,trans.pair,transect_pair,transect,altitude_scaled)])
 preds_shannon[,shannon := Estimate]
+
 (shannon_pred_plot <- ggplot(data_richness,aes (x = transect_pair, y = shannon , fill = treatment,group =treatment))+
     theme_classic()+
     theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1))+
@@ -148,5 +150,8 @@ preds_shannon[,shannon := Estimate]
     geom_point(position = position_dodge(0.6),data = preds_shannon, aes(y = Estimate),pch = 21, size = 4)+
     scale_color_manual(values = trail_color <- c("#27A81E", "#DEBF50"), labels = trail_labs <- c("Far (<5m)","Trampled"))+
     scale_fill_manual(values = trail_color)+
-    labs(x = "Transect pair", y = "Hannon diversity index", color = lab_trt <- "Position along\nthe trail",fill = lab_trt)
+    labs(x = "Transect pair", y = "Shannon diversity index", color = lab_trt <- "Position along\nthe trail",fill = lab_trt)
 )
+
+ggsave(file.path("figure","Fig_Shannon_transect.jpg"),shannon_pred_plot,unit = "cm",width = 18,height = 11,dpi = 300)
+
--- scripts/02_Species_cover.R
+++ scripts/02_Species_cover.R
@@ -1,16 +1,20 @@
-library(data.table)
-library(brms)
-library(ggplot2)
-library(stringr)
-
-## how does trampling affect species cover ?
-# some species has never been recorded, only 0, removing them
+##### packages ####
+library(data.table) # faster data.frame
+library(brms) # bayesian model fitting using stan
+library(ggplot2) # plots
+library(stringr) # string manipulation
+library(vegan) # diversity indices computation
+#### how does trampling affect species cover ? ####
 data_cover <- data_long
-data_cover[,species := variable];data_cover[,variable := NULL]
+data_cover[,species := variable];data_cover[,variable := NULL] # renaming
 data_cover[,cover := value];data_cover[,value := NULL]
 
+
+# some species has never been recorded, only 0, removing them
 data_cover[,never_found := all(cover == 0),by = species]
 data_cover <- data_cover[never_found == F,]; data_cover[,never_found := NULL]
+
+## site naming and scaling altitude 
 data_cover[,altitude_scaled := scale (altitude)]
 data_cover[,site := substr(transect,1,2)]
 data_cover[,transect_pair := paste0(site,"_",str_pad(trans.pair, width=2, side="left", pad="0"))]
@@ -19,32 +23,34 @@ data_cover[,transect_pair := paste0(site,"_",str_pad(trans.pair, width=2, side="
 data_cover[cover != 0 ,length(unique(species)),by = .(treatment,transect_pair)]
 data_cover[cover != 0 ,length(unique(species)),by = .(transect_pair)]
 
+## ASSUMPTION: A species is in the local pool of a pair of transect if it has been recorded once
+## CONSEQUENCE: species that has not been recorded once at a pair are REMOVED from the data
+## CONSEQUENCE: Species recorded once in a pair will be 0 (true absence) when not recorded in other plots of the pair
 sp_pool_of_site <- data_cover[cover != 0 ,.(species = unique(species)),by = .(transect_pair)]
 
 ## remove from the sites species that has never been found in the site / transect
 data_cover_trim <- merge(data_cover,sp_pool_of_site)
 data_cover_trim[,species := droplevels(species)]
-data_cover_trim[,cover_1 := ceiling(cover)]
-data_cover_trim[,cover_1 := cover / 100]
+data_cover_trim[,cover_1 := cover / 100] # beta distrib needs cover between 0 and 1 
 
+##looks like zero inflated beta
+ggplot(data_cover_trim,aes( x = cover_1, fill = species))+geom_histogram(binwidth = 1)+theme_classic()+facet_wrap(~ treatment)
 
-ggplot(data_cover_trim,aes( x = cover))+geom_histogram(binwidth = 1)+theme_classic()
-ggplot(data_cover_trim,aes( x = cover, fill = species))+geom_histogram(binwidth = 1)+theme_classic()+facet_wrap(~ treatment)
+#### Modelling 
 
-prior_cover <- set_prior("normal(-3,2)",class = "Intercept",dpar = "mu")
-prior_cover <- c(prior_cover,set_prior("normal(1,1)",class = "Intercept",dpar = "zi"))
-prior_cover <- c(prior_cover,set_prior("student_t(3, 0.5, 0.5)", class = "sd", group = "trans.pair"))
-#prior_cover <- c(prior_cover,set_prior("student_t(3, 0.5, 0.5)", class = "sd", group = "site"))
-prior_cover <- c(prior_cover,set_prior("student_t(3, 0.5, 0.5)", class = "sd", group = "transect"))
-prior_cover <- c(prior_cover,set_prior("normal(10,20)", class = "phi" ))
+## setting the prior knowledge: 
 
-formula_model <- bf(cover_1 ~ treatment + (1|trans.pair) + (1|transect) + (treatment|species) + (1|transect:species)+ (1|trans.pair:species),
-   zi ~   treatment + (1|trans.pair) + (1|transect) + (treatment|species) + (1|transect:species)+ (1|trans.pair:species))
+prior_cover <- set_prior("normal(-3,1)",class = "Intercept",dpar = "mu") # I expect the average plant to be tiny : cover of inv_logit(-3): 0.05 , 5 % cover
+prior_cover <- c(prior_cover,set_prior("normal(10,10)", class = "phi" )) # Setting the shape parameter to be wide to accommodate for the data dispertion
+# those are wide prior, so nuch much informations given here
 
-model_cover <- brm(formula_model,
+formula_model <- bf(cover_1 ~ treatment + (1|trans.pair) + (1|transect) + (1 + treatment|species) + (1|transect:species)+ (1|trans.pair:species),
+   zi ~   treatment + (1|trans.pair) + (1|transect) + (1 + treatment|species) + (1|transect:species)+ (1|trans.pair:species))
+
+model_cover <- brm(formula_model, # takes a while to compute 
                    data = data_cover_trim,
                    family = zero_inflated_beta(),
-                   backend = "cmdstanr", 
+                   backend = "cmdstanr", # "rstan" if not working
                    iter = 1000,
                    prior = prior_cover ,
                    warmup = 200,
@@ -55,52 +61,96 @@ model_cover <- brm(formula_model,
                    init = 0)
 
 
-
+## distribution is fitting as well as possible, but the not-continuous cover is making it harder to see
 pp_check(model_cover)+coord_cartesian(xlim = c(0,0.5))
 summary(model_cover)
-plot(model_cover)
-data_cover[,sum(cover != 0),by = .(treatment,species)]
+plot(model_cover)## parameters converged because the caterpillar are fuzzy
 
-sjPlot::plot_model(model_cover,type = "pred",terms = c("species","treatment"))
-sjPlot::plot_model(model_cover,type = "pred",terms = c("treatment"), dpar = 'mu')
-sjPlot::plot_model(model_cover,type = "pred",terms = c("treatment"), dpar = 'hu')
+## rough plots to look at the effect of trampling per species
+conditional_effects(model_cover,effects = c("species:treatment"),re_formula = ~ (1 + treatment|species))
+conditional_effects(model_cover,effects = c("species:treatment"),re_formula = ~ (1 + treatment|species),dpar = "mu")
+conditional_effects(model_cover,effects = c("species:treatment"),re_formula = ~ (1 + treatment|species),dpar = "hu")
 
-conditional_effects(model_cover,effects = c("species:treatment"),re_formula = ~ (1|species))
-conditional_effects(model_cover,effects = c("species:treatment"),re_formula = ~ (1|species),dpar = "mu")
-conditional_effects(model_cover,effects = c("species:treatment"),re_formula = ~ (1|species),dpar = "hu")
+## rough plots to look at the general effect of trampling
+conditional_effects(model_cover,effects = c("treatment"),re_formula = NA) # average of prob * cover
+conditional_effects(model_cover,effects = c("treatment"),re_formula = NA,dpar = "mu") # cover
+conditional_effects(model_cover,effects = c("treatment"),re_formula = NA,dpar = "zi")  # prob of absence
 
-conditional_effects(model_cover,effects = c("treatment"),re_formula = NA)
-conditional_effects(model_cover,effects = c("treatment"),re_formula = NA,dpar = "mu")
-conditional_effects(model_cover,effects = c("treatment"),re_formula = NA,dpar = "zi")
-
-preds_cover <- fitted(model_cover,
-                      newdata =data_cover_trim[,.N, by = .(treatment,site,trans.pair,transect,transect_pair,species)],
-                      re_formula = ~ (1|trans.pair) + (1|transect) + (treatment|species) + (1|transect:species)+ (1|trans.pair:species))
-
-preds_cover <- cbind(data_cover_trim[,.N, by = .(treatment,site,trans.pair,transect,transect_pair,species)],preds_cover)
-
-ggplot(data_cover_trim[cover!=0,],aes( x = species, y = cover,group = treatment,fill = treatment))+
-  geom_point(pch = 21, size = 2,alpha = 0.5,
-              position = position_jitterdodge(0.2,0.1,0.6))+
-  #facet_wrap(~site, scale = "free_x",nrow = 3)+
-  theme_classic()
-
+## trick for alphabetical order
 data_cover_trim[,species_order := as.character(species)]
-new_frame <- data_cover_trim[,.N ,by = .(treatment,species,species_order),]
+
+## getting prediction from the model, only asking for species specific random effects, everything else assumed average
+new_data_for_pred <- data_cover_trim[,.N ,by = .(treatment,species,species_order),]
+
 pred_sp <- cbind(new_frame,fitted(model_cover,
-                  newdata =new_frame,
-                  re_formula = ~ (1|species) ) * 100)
+                  newdata =new_data_for_pred, # no dpar argument : the whole model prob * cover, dpar = "mu" cover, dpar= "zi" probability of absence 
+                  re_formula = ~ (1+treatment|species) ) * 100)
 
-
+## plots! can be adapted to show full model, just zi, just mu
 (figure_pred_sp <- ggplot(data_cover_trim[,],aes( x = species_order, y = cover,group = treatment,fill = treatment, color = treatment))+
   geom_point( size = 1,alpha = 0.5, position = position_jitterdodge(0.2,0.1,0.6),fill = NA)+
   geom_pointrange(aes(y = Estimate,ymin = Q2.5,ymax = Q97.5 ),pred_sp,
                   color = "grey5",pch = 21,size = 0.5,lwd = 0.5,lineend = "round",position = position_dodge(0.6))+
- theme_classic()+
-   theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1),legend.position = c(0.2,0.8))+
+  theme_classic()+
+    scale_y_continuous(limits =  c(0,50),oob = scales::squish)+ ##squishing the very high cover, can be removed
+   theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1),
+         legend.position = c(0.1,0.8),
+         axis.title.x = element_blank())+
   
   scale_color_manual(values = trail_color <- c("#27A81E", "#DEBF50"), labels = trail_labs <- c("Far (<5m)","Trampled"))+
   scale_fill_manual(values = trail_color, labels = trail_labs )+
     labs(x = "", y = "Cover (%)",color = lab_trt,fill = lab_trt ))
 
+# export
 ggsave(file.path("figure","Fig_cover_sp.jpg"),figure_pred_sp,unit = "cm",width = 18,height = 14,dpi = 300,scale = 1.25)
+
+## getting the raw draws from the model to compute delta predictions
+new_data_for_pred <- data_cover_trim[,.N ,by = .(treatment,species,species_order),]
+
+pred_sp <- data.table(fitted(model_cover,
+       newdata =new_data_for_pred, # no dpar argument : the whole model prob * cover, dpar = "mu" cover, dpar= "zi" probability of absence 
+       re_formula = ~ (1+treatment|species), summary = F) * 100)
+pred_sp <- t(pred_sp)
+pred_sp <- cbind(new_frame[,],pred_sp)
+
+pred_sp <- melt(pred_sp,id.vars = c("treatment","species","species_order","N"))
+pred_sp[,draws_id := variable ] ; pred_sp[,variable := NULL ]  
+pred_sp[,pred_cover := value ] ; pred_sp[,value := NULL ]  
+
+pred_sp <- pred_sp[,.(delta_cover = diff(-pred_cover)),by = .(species,species_order,draws_id,N)]
+pred_sp_summarized <- pred_sp[,.(Estimate = mean(delta_cover),
+                                 Q2.5 = quantile(delta_cover,probs = 0.025),
+                                 Q97.5  = quantile(delta_cover,probs = 0.975),
+                                 prob_of_being_signif = mean(delta_cover<0)),by = .(species,species_order,N)]
+
+
+pred_sp_summarized[,signif := ifelse(sign(Q2.5) == sign(Q97.5),"Signif","Non")]
+pred_sp_summarized[,signif_2way := ifelse(sign(Q2.5) == sign(Q97.5) & sign(Estimate) == -1,"Decline",
+                                             ifelse(sign(Q2.5) == sign(Q97.5) & sign(Estimate) == 1,"Increase","Non signif"))]
+
+(ggplot(pred_sp_summarized,aes( x= reorder(species_order,Estimate), y= Estimate, ymin = Q2.5, ymax  = Q97.5,color = signif))+
+    geom_hline(yintercept = 0, lty = 2, color = "grey20")+
+    geom_pointrange()+
+    theme_classic()+
+    theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1),
+          legend.position = c(0.7,0.3),
+          axis.title.x = element_blank())+
+    scale_color_manual(values = c("grey60","grey10"))+
+    labs( y= "Distribution of Δcover")
+  )
+
+
+
+s
+####alternative probabilistic representation
+(ggplot(pred_sp_summarized,aes( x= reorder(species_order,prob_of_being_signif), y= prob_of_being_signif,color = signif))+
+    geom_hline(yintercept = 0, lty = 2, color = "grey20")+
+    geom_point()+
+    theme_classic()+
+    theme(axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1),
+          legend.position = c(0.7,0.3),
+          axis.title.x = element_blank())+
+    scale_color_manual(values = c("grey60","grey10"))+
+    labs( y= "Distribution of Δcover")
+)
+
